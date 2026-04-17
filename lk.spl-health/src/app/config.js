a0aa90d3c64f519fd1a021adbe3282d7d5c67051
AndreyZver <andrey1000vrn@gmail.com>
2026-04-17 22:50:30 +0300

    fix: .gitcommit

---
+++ lk.spl-health/src/app/config.js
@@ -0,0 +1,8 @@
+export const baseApiUrl = 'http://spl-health/api';
+
+// *********************************************************************
+//                           OpenMeetings config
+//*********************************************************************
+export const baseApiOmUrl = 'http://192.168.234.132:5080/openmeetings';
+export const OM_ADMIN_LOGIN = 'andreyankudinovandrey@yandex.ru';
+export const OM_ADMIN_PASSWORD = '#NewLife2023!';
